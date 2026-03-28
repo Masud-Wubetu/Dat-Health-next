26442a5dd56219e8fd46366c1bd597893895a2ac
Masud <wubetumasud@gmail.com>
2026-03-29 00:22:28 +0300

    create transporter

---
+++ lib/email-services.ts
@@ -0,0 +1,14 @@
+import nodemailer from 'nodemailer'
+import fs from 'fs'
+import path from 'path'
+
+// Create transporter
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT || '587'),
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+})
